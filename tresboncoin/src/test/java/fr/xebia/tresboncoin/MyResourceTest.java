502f47d5dc4b8ab7aa89c24e93b2d61944e2bdb6
Christophe <christophe.pele@gmail.com>
2013-09-04 11:41:12 +0200

    Remove bogus import

--- tresboncoin/src/test/java/fr/xebia/tresboncoin/MyResourceTest.java
+++ tresboncoin/src/test/java/fr/xebia/tresboncoin/MyResourceTest.java
@@ -2,18 +2,9 @@ package fr.xebia.tresboncoin;
 
 import org.fest.assertions.Assertions;
 import org.junit.Test;
-import sun.jvm.hotspot.utilities.Assert;
 
 import java.io.IOException;
-import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: cp
- * Date: 04/09/13
- * Time: 10:51
- * To change this template use File | Settings | File Templates.
- */
 public class MyResourceTest {
     @Test
     public void shouldGet35ResultsFromHome() throws IOException {
