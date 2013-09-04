d6ff8fa15297ea3904a4179b015d1171f94a7999
Christophe <christophe.pele@gmail.com>
2013-09-04 10:34:50 +0200

    Access jersey resource from angular

--- tresboncoin/src/main/webapp/app/scripts/controllers/main.js
+++ tresboncoin/src/main/webapp/app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('webappApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        // Call backend
+        $http.get('../webresources/myresource').success(function(data) {
+            $scope.awesomeThings.push(data)
+        })
+    });
